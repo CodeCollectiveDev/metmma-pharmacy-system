123af82442d7403c243c7831836cf1ecb86b37b8
Mike Prosper Kamanga <mikekamanga59@gmail.com>
2026-09-17 23:46:11 +0200

    fix: secure expiring products query
    (cherry picked from commit 37601b1196bd57edb62be7453bbd78502f57e3cf)

--- backend/api/routes/productsRoutes.js
+++ backend/api/routes/productsRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productsController')
-const { createProductSchema, updateProductSchema, validateProduct } = require('../validators/productValidator');
+const {
+  createProductSchema,
+  updateProductSchema,
+  validateProduct,
+  validateExpiringProductsQuery
+} = require('../validators/productValidator');
 const { authenticate, authorize, ROLES } = require('../../middleware/roleMiddleware');
 
 router.get('/', authenticate, productController.getAllProducts);
 router.get('/low-stock', authenticate, productController.getLowStockProducts);
-router.get('/expiring', authenticate, productController.getExpiringProducts);
+router.get('/expiring', authenticate, validateExpiringProductsQuery, productController.getExpiringProducts);
 router.get('/:id', authenticate, productController.getProductById);
 
 router.post('/', authenticate, authorize(ROLES.SUPER_ADMIN, ROLES.PHARMACIST_MANAGER, ROLES.STORE_MANAGER, ROLES.PHARMACIST), validateProduct(createProductSchema), productController.createProduct);
